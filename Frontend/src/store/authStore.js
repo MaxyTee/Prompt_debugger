db6e17ebf773659260bba0241f96405b43254193
maxy <166556889+MaxyTee@users.noreply.github.com>
2025-11-26 17:12:07 +0100

    Update API URL in authStore.js

--- Frontend/src/store/authStore.js
+++ Frontend/src/store/authStore.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "artistic-beauty-production.up.railway.app/api/auth";
 
 axios.defaults.withCredentials = true;
 
