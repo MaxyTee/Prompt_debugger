cbf91fa9a72e2ca4000c9ec570dee51ade5eed5e
unknown <mariamadebimpetairu@gmail.com>
2025-12-02 21:03:52 +0100

    update

--- Frontend/src/store/messageStore.js
+++ Frontend/src/store/messageStore.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { create } from "zustand";
 
-const API_URL = `${import.meta.VITE_API_URL}`;
+const API_URL = `${import.meta.env.VITE_API_URL}`;
 export const useMessageStore = create((set, get) => ({
   message: null,
   messagesDB: [],
